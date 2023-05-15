5063671015976cc0668a7f95d31d2eab4b3804bf
yadharis-perdomou <123768363+yadharis-perdomou@users.noreply.github.com>
2023-05-15 12:39:30 -0500

    topillo

---
+++ app.js
@@ -0,0 +1,29 @@
+const cuadrado = document.querySelectorAll(".cuadrado");
+const tiempofaltante = document.querySelector("#tiempo");
+let puntaje = document.getElementById("puntaje");
+
+let resultado = 0;
+let tiempoactual = tiempofaltante.textContent;
+
+
+function cuadradoazar {
+  cuadrado.forEach (nombredeclase => {
+    nombredeclase.classList.remove ("topo");
+  })
+  let posicionalazar = cuadrado [Math.floor(Math.random() * 9)]
+  posicionalazar.classList.add("topo");
+  posiciontopo.posicionalazar.Id
+}
+
+
+cuadrado.forEach(identificador =>{
+  identificador.addEventListener("click"()=> {
+    if (identificador.Id === posiciontopo) {
+  resultado = resultado +1;
+  puntaje.textContent = resultado; 
+  posiciontopo= null;
+   }
+ })
+})
+  
+  
