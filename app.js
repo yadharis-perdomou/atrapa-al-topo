c3960f36f937e25cbb81128610f767222717ca0d
yadharis-perdomou <123768363+yadharis-perdomou@users.noreply.github.com>
2023-06-09 11:27:16 -0500

    fin

--- app.js
+++ app.js
@@ -1,45 +1,44 @@
-const cuadrado = document.querySelectorAll(".cuadrado");
+const cuadro = document.querySelectorAll(".cuadrado");
 const tiempofaltante = document.querySelector("#tiempo");
 let puntaje = document.getElementById("puntaje");
 
 let resultado = 0;
 let tiempoactual = tiempofaltante.textContent;
 
-
-function cuadradoazar {
-  cuadrado.forEach (nombredeclase => {
-    nombredeclase.classList.remove ("topo");
-  })
-  let posicionalazar = cuadrado [Math.floor(Math.random() * 9)]
+function cuadroazar() {
+  cuadro.forEach((nombredeclase) => {
+    nombredeclase.classList.remove("topo");
+  });
+  let posicionalazar = cuadro[Math.floor(Math.random() * 9)];
   posicionalazar.classList.add("topo");
-  posiciontopo.posicionalazar.Id
+
+  posiciontopo = posicionalazar.id;
 }
 
-
-cuadrado.forEach(identificador =>{
-  identificador.addEventListener("click"()=> {
-    if (identificador.Id === posiciontopo) {
-  resultado = resultado +1;
-  puntaje.textContent = resultado; 
-  posiciontopo= null;
-   }
- });
+cuadro.forEach((identificador) => {
+  identificador.addEventListener("click", () => {
+    if (identificador.id === posiciontopo) {
+      resultado = resultado + 1;
+      puntaje.textContent = resultado;
+      posiciontopo = null;
+    }
+  });
 });
 
-function movertopo (){
-  tiempotopo = setInterval (cuadradoazar,700);
+function movertopo() {
+  tiempotopo = setInterval(cuadroazar, 700);
 }
 movertopo();
 
-function cuentaregresiva (){
+function cuentaregresiva() {
   tiempoactual--;
   tiempofaltante.textContent = tiempoactual;
-  if (tiempoactual=== 0){
+  if (tiempoactual === 0) {
     clearInterval(idtiempo);
     clearInterval(tiempotopo);
-    alert ("se acabo el tiempo tu puntaje fue:" + resultado + "topos atrapados:");
+    alert(
+      "se acabó el tiempo, tu puntaje fue de " + resultado + " topos atrapados"
+    );
   }
-}  
-
-
-  
+}
+let idtiempo = setInterval(cuentaregresiva, 1000);
